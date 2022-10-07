b8d8cacb088b02be5dd0048673e85f2f8fdb5e63
l o l i <105023532+lolafioramanti@users.noreply.github.com>
2022-10-06 22:27:28 -0300

    vuelve test
    me habia confundido y subi el de la alu aca

--- testbench_moduloAritmetico.vhd
+++ testbench_moduloAritmetico.vhd
@@ -1,107 +1,94 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all; 
 
-entity ALU_Nbits_test is
-end ALU_Nbits_test;
+ENTITY SumArit_Nbits_test IS
+END SumArit_Nbits_test;
 
-architecture Behavioral of ALU_Nbits_test is
-
-	constant zero : integer := 4;
-	constant n : integer := 4;
-
-	component ALU_Nbits is 
-	generic (n : integer := 4);
-	port(
-    	nro1,nro2: in signed(N-1 downto 0);
-        selec: in std_logic_vector (1 downto 0);
-        resul: out signed(N-1 downto 0);
-        flags: out std_logic_vector(3 downto 0);
-         );
+architecture Behavioral of SumArit_Nbits_test is
+	component SumArit_Nbits
+    generic(n : integer := 4);
+    port(
+        a, b: in signed(n-1 downto 0);
+        op: in std_logic;
+        r: out signed(n-1 downto 0);
+        zcvn: out std_logic_vector(3 downto 0)
+    );
     end component;
 
-signal nro1_test, nro2_test: signed (N-1 downto 0) := "0000";
-signal selec_test: std_logic_vector (1 downto 0) := "00";
-signal resul_test: signed (N-1 downto 0);
-signal flags_test: std_logic_vector (3 downto 0);
+	signal test_a, test_b: signed (n-1 downto 0) := '0000';
+    signal test_op std_logic := '0';
+    signal r_test signed (n-1 downto 0) := '0000';
+    signal test_zcvn std_logic_vector(3 downto 0) := '0000';
 
 begin
+	SumArit: entity work.SumArit_Nbits(Behavioral)
+    port map(
+    a => test_a;
+    b => test_b;
+    op => test_op;
+    r => r_test;
+    zcvn = > test_zcvn
+    );
 
-ALU: ALU_Nbits port map(
-	nro1=> nro1_test,
-        nro2=>nro2_test,
-		selec=>selec_test,
-        resul=> resul_test,
-        flags=>flags_test
-        );
-
-process
+process 
 begin
-	
-    nro1_test <= "0001";
-        nro2_test <= "0001";
-        selec_test <= "00";
-        wait for 1 ns;
-        assert (resul_test = "0010") report "Fallo Caso 1" severity failure;
-        assert (flags_test = "0000") report "Fallo Caso 1 (flag)" severity failure;
-        
-        nro1_test <= "0101";
-        nro2_test <= "0101";
-        selec_test <= "00";
-        wait for 1 ns;
-        assert (resul_test = "1010") report "Fallo Caso 2" severity failure;
-        assert (flags_test = "0011") report "Fallo Caso 2 (flag)" severity failure;
-        
- 		nro1_test <= "0101";
-        nro2_test <= "0101";        
-        selec_test <= "01";
-        wait for 1 ns;
-        assert (resul_test = "0000") report "Fallo Caso 3" severity failure;
-        assert (flags_test = "1000") report "Fallo Caso 3 (flag)" severity failure;
-        
-        nro1_test <= "1000";
-        nro2_test <= "0111";
-        selec_test <= "00";
-        wait for 1 ns;
-        assert (resul_test = "1111") report "Fallo Caso 4" severity failure;
-        assert (flags_test = "0001") report "Fallo Caso 4 (flag)" severity failure;
-        
-        nro1_test <= "1000";
-        nro2_test <= "0111";
-        selec_test <= "01";
-        wait for 1 ns;
-        assert (resul_test = "0001") report "Fallo Caso 5" severity failure;
-        assert (flags_test = "0000") report "Fallo Caso 5 (flag)" severity failure;
-        
-        nro1_test <= "0110";
-        nro2_test <= "1010";
-        selec_test <= "00";
-        wait for 1 ns;
-        assert (resul_test = "0000") report "Fallo Caso 6" severity failure;
-        assert (flags_test = "1100") report "Fallo Caso 6 (flag)" severity failure;
-        
-        nro1_test <= "0110";
-        nro2_test <= "1010";
-        selec_test <= "01";
-        wait for 1 ns;
-        assert (resul_test = "1100") report "Fallo Caso 7" severity failure;
-        assert (flags_test = "0101") report "Fallo Caso 7 (flag)" severity failure;
-        
-        nro1_test <= "1101";
-        nro2_test <= "1100";
-        selec_test <= "10";
-        wait for 1 ns;
-        assert (resul_test = "1100") report "Fallo Caso 8" severity failure;
-        assert (flags_test = "0101") report "Fallo Caso 8 (flag)" severity failure;
-                
-        nro1_test <= "1101";
-        nro2_test <= "1101";
-        selec_test <= "11";
-        wait for 1 ns;
-        assert (resul_test = "1101") report "Fallo Caso 9" severity failure;
-        assert (flags_test = "1110") report "Fallo Caso 9 (flag)" severity failure;
-    
-    assert false report "Test done" severity note;
+
+    test_a <= '';
+    test_b <= '';
+    test_sel <= '';
+    wait for 1 ns;
+    assert (r_test='', test_zcvn='') report "Fallo caso 0" severity error;
+
+    test_a <= '';
+    test_b <= '';
+    test_sel <= '';
+    wait for 1 ns;
+    assert (r_test='', test_zcvn='') report "Fallo caso 0" severity error;
+
+    test_a <= '';
+    test_b <= '';
+    test_sel <= '';
+    wait for 1 ns;
+    assert (r_test='', test_zcvn='') report "Fallo caso 0" severity error;
+
+    test_a <= '';
+    test_b <= '';
+    test_sel <= '';
+    wait for 1 ns;
+    assert (r_test='', test_zcvn='') report "Fallo caso 0" severity error;
+
+    test_a <= '';
+    test_b <= '';
+    test_sel <= '';
+    wait for 1 ns;
+    assert (r_test='', test_zcvn='') report "Fallo caso 0" severity error;
+
+    test_a <= '';
+    test_b <= '';
+    test_sel <= '';
+    wait for 1 ns;
+    assert (r_test='', test_zcvn='') report "Fallo caso 0" severity error;
+
+    test_a <= '';
+    test_b <= '';
+    test_sel <= '';
+    wait for 1 ns;
+    assert (r_test='', test_zcvn='' ) report "Fallo caso 0" severity error;
+
+    test_a <= '';
+    test_b <= '';
+    test_sel <= '';
+    wait for 1 ns;
+    assert (r_test='', test_zcvn='') report "Fallo caso 0" severity error;
+
+    test_a <= '';
+    test_b <= '';
+    test_sel <= '';
+    wait for 1 ns;
+    assert (r_test='', test_zcvn='') report "Fallo caso 0" severity error;
+
+    assert false report "Test terminado" severity note;
     wait;
   end process;
 end;
